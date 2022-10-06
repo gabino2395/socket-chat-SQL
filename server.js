b0e5e1b27c37487872aae45841c43f4c53622af2
gabino mendez <gabino_mendez@hotmail.com.ar>
2022-10-06 20:16:35 -0300

    login con redes sociales

--- server.js
+++ server.js
@@ -6,18 +6,96 @@ const { Server: ServerIo } = require('socket.io')
 const Contenedor = require('./Contenedor')
 const products = new Contenedor('products')
 const users = new Contenedor('users')
+const initializePassport = require('./passport-config')
+const MongoStore = require('connect-mongo');
 
 const httpServer = new ServerHttp(app)
 const io = new ServerIo(httpServer)
-//hola
 
+
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config()
+}
+const bcrypt = require('bcrypt')
+const passport = require('passport')
+const flash = require('express-flash')
+const session = require('express-session')
+const methodOverride = require('method-override')
+//mongo-connect
+const mongoConfig = {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+}
+
+initializePassport(
+    passport,
+    email => users.find(user => user.email === email),
+    id => users.find(user => user.id === id)
+)
+app.use(flash())
+app.use(session({
+	secret:  '123456',
+	resave: true,
+	saveUninitialized: true,
+
+	store: MongoStore.create({ mongoUrl: `mongodb+srv://gabo2395:lebron23JAMES@cluster0.78xslog.mongodb.net/?retryWrites=true&w=majority`, mongoOptions: mongoConfig })
+
+}))
 //app
-app.use(express.static('public'))
+app.set('view-engine', 'ejs')
 
+app.use(express.static('public'))
+app.get('/', checkAuthenticated, (req, res) => {
+    res.render('index.ejs', { name: req.user.name })
+})
+
+app.get('/login', checkNotAuthenticated, (req, res) => {
+    res.render('login.ejs')
+})
 app.get('/', (req, res) => {
     res.sendFile('index.html', { root: __dirname })
 })
+app.post('/login', checkNotAuthenticated, passport.authenticate('local', {
+    successRedirect: '/',
+    failureRedirect: '/login',
+    failureFlash: true
+}))
 
+app.get('/register', checkNotAuthenticated, (req, res) => {
+    res.render('register.ejs')
+})
+app.post('/register', checkNotAuthenticated, async (req, res) => {
+    try {
+        const hashedPassword = await bcrypt.hash(req.body.password, 10)
+        users.push({
+            id: Date.now().toString(),
+            name: req.body.name,
+            email: req.body.email,
+            password: hashedPassword
+        })
+        res.redirect('/login')
+    } catch {
+        res.redirect('/register')
+    }
+})
+app.delete('/logout', (req, res) => {
+    req.logOut()
+    res.redirect('/login')
+})
+
+function checkAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) {
+        return next()
+    }
+
+    res.redirect('/login')
+}
+function checkNotAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) {
+        return res.redirect('/')
+    }
+    next()
+}
 
 io.on('connection', async (socket) => {
     const productos = await products.getAll()
